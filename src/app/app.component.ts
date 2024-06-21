54865b1f1158da3e2c848ad05a360c1c4523697f
propio14 <143516057+propio14@users.noreply.github.com>
2024-06-21 15:07:52 -0400

    PROYECTO ANGULAR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import FooterComponent from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Mi-Primer-Proyecto-Angular1';
+
+ 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', redirectTo: '/inicio', pathMatch: 'full' },
+    {path:'inicio',component:InicioComponent},
+    {path:'acercade',component:AcercadeComponent},
+    { path: 'contacto', component: ContactoComponent }
+];
